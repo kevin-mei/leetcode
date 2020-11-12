9a583422e3fd7dd1e2ec72a86bcf103320fabe1e
meiyao(梅耀) <meiyao@HOLD.FOUNDER.COM>
2020-11-12 10:26:33 +0800

    更新1，79题解

--- code/1.两数之和.cpp
+++ code/1.两数之和.cpp
@@ -30,10 +30,48 @@
  */
 
 // @lc code=start
+#include "pch.h"
+
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
+    // 平凡的算法会超时
+    vector<int> twoSum_timeLimit(vector<int>& nums, int target) {
+        vector<int> res;
+        for(int i=0; i < nums.size();i++)
+        {
+            for(int j = i+1; j < nums.size();j++)
+            {
+                if(target == nums[i] + nums[j])
+                {
+                    res.push_back(i);
+                    res.push_back(j);
+                }
+            }
+        }
+        return res;
+    }
 
+    vector<int> twoSum(vector<int>& nums, int target) {
+        // 用额外n的空间存储target - nums[i] 的差值
+        // map_data 保存 (差值，下标) 数组元素有可能相同，就会有相同的差值，这时候i覆盖
+        // 题中说只有一个正确答案，这时候i覆盖的情况，也不会是正确答案，如果是，则不只答案了
+        std::unordered_map<int,int> map_data;
+        // 遍历一边，一边判断当前nums[i]是否在map_data的keys中能找到，能找到说明是正确答案，找到对应value，就是上一个数的下标
+        // 没找到就map_data[差值] = i;把<差值，下标>保存到map中
+        vector<int> res;
+        for(int i=0; i < nums.size();i++)
+        {
+           if(map_data.find(nums[i]) != map_data.end())
+           {
+               res.push_back(map_data[nums[i]]);
+               res.push_back(i);
+           }else
+           {
+               int dest = target -nums[i];
+               map_data[dest] = i;
+           }
+        }
+        return res;
     }
 };
 // @lc code=end
--- code/79.单词搜索.cpp
+++ code/79.单词搜索.cpp
@@ -51,21 +51,57 @@ class Solution {
 public:
     bool exist(vector<vector<char>>& board, string word) {
         bool res = false;
-        // 深度优先搜索遍历  使用stack
+        // 深度优先搜索遍历 这里考虑用 递归的方式
+        // 新建一个函数bool check(i,j,k) 标识从board[i][j]出发，能否访问到word[k]直到word结尾
+        // 外层调用它，需要从从第一个单元格开始，从从左至右，上到下开始访问，调用check(i,j,0) 
+        // 如果有返回true的，则说明存在，直接退出循环
         int m = board.size();
         int n = board[0].size();
-        stack<char> stack_data;
         // 初始化一个m*n bool 二维数组，表示单元格是否访问过
         vector<vector<bool>> visited(m, vector<bool>(n, false));
 
-        // 栈里面存什么？
-        // 存访问过的数据，从第一个节点开始，从从左至右，上到下开始访问，直到访问到word的字符，存如栈中
-        // 然后栈不为空，且word没找完的情况下，从栈顶元素的上下左右找，是否存在word下一个字符
-        // 是，则入栈，进入下一个循环，visited[i][j] = true;否，则弹栈,对应的visited[i][j]=false;
-        
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                res = check(board, visited, word, i, j, 0);
+                if (res)
+                {
+                    return res;
+                }
+            }
+        }
+
+        return res;
+    }
 
 
-
+    bool check(vector<vector<char>> &board, vector<vector<bool>> &visited, string word, int i, int j, int k)
+    {
+        bool res = false;
+        // 先比较board[i][j]和word[k],不相等则返回false,  如果visited[i][j]已经访问过了，也返回false
+        // 相等时先判断是否k==word.size()-1，是最后一个，说明完整找到word，返回true, 不是最后一个，则先visited[i][j]=true
+        // 然后四个方向各调用下check(new_i,new_j,k+1);有true时说明找到，直接返回
+        if(board[i][j] != word[k] || visited[i][j])
+            return false;
+        else if(k==word.size()-1) // 这里就暗含board[i][j] == word[k]
+            return true;
+        // 下面是board[i][j] == word[k] 且k不是word最后一个元素下标的处理
+        visited[i][j] = true;
+        vector<pair<int,int>> directors = {{0,1},{0,-1},{-1,0},{1,0}}; // 定义4个方向 上下左右
+        for(pair<int,int> director : directors)
+        {
+            int new_i = i+ director.first, new_j = j+director.second;
+            // 判断新坐标的合法性 且visited[new_i][new_i]没有被访问过
+            if(new_i >= 0 && new_i < board.size() && new_j >= 0 && new_j < board[0].size())
+            {
+                res = check(board, visited, word, new_i, new_j, k+1);
+                if(res)
+                    return res;
+            }
+        }
+        // 如果四个方向都走完了，没有找到路径，会自然退出循环,说明从[i][j]开始没法找到，这时候visited[i][j] = false；做别人的临结点访问
+        visited[i][j] = false;
         return res;
     }
 
