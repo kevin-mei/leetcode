a9a5c53d1eff03c3b3c3ea6aafed4d1d4c8689f2
Kevin-Mei <Mei_Yao_1@163.com>
2021-05-10 07:42:29 +0800

    两数之和增加双指针算法处理

--- code/1.两数之和.cpp
+++ code/1.两数之和.cpp
@@ -30,7 +30,7 @@
  */
 
 // @lc code=start
-#include "pch.h"
+//#include "pch.h"
 
 class Solution {
 public:
@@ -51,7 +51,7 @@ public:
         return res;
     }
 
-    vector<int> twoSum(vector<int>& nums, int target) {
+    vector<int> twoSum_hash(vector<int>& nums, int target) {
         // 用额外n的空间存储target - nums[i] 的差值
         // map_data 保存 <差值，下标> 数组元素有可能相同，就会有相同的差值，这时候i覆盖
         // 题中说只有一个正确答案，这时候i覆盖的情况，也不会是正确答案，如果是，则不只一个答案了
@@ -73,6 +73,45 @@ public:
         }
         return res;
     }
+
+    vector<int> twoSum(vector<int>& nums, int target) {
+        // 先对nums进行排序，从小到大，双指针，sum = nums[left]+nums[right]; 
+        // sum <target, 那left++，反之则right--，直到相等；
+        vector<int> res;
+        vector<pair<int, int>> nums_vect;
+        for(int i =0; i < nums.size(); i++)
+        {
+            nums_vect.push_back({nums[i], i});
+        }
+
+        auto compare=[](pair<int, int> &a, pair<int, int> &b){ return a.first < b.first; };
+        sort(nums_vect.begin(), nums_vect.end(), compare);
+        int left =0, right = nums_vect.size()-1;
+        while (left < right)
+        {
+            int sum = nums_vect[left].first +nums_vect[right].first;
+            if(sum < target)
+                left++;
+            else  if(sum > target)
+                right--;
+            else{
+                res.push_back(nums_vect[left].second);  
+                res.push_back(nums_vect[right].second); 
+                break; 
+            }
+        }
+        return res;
+    }
 };
+
+// int main()
+// {
+//     vector<int> nums = {2,7,11,15};
+//     Solution sol;
+//     int target = 9;
+//     vector<int> res = sol.twoSum(nums, target);
+//     cout << res.size() << endl;
+//     return 0;
+// }
 // @lc code=end
 
